7b14ce1971324c1167c08d722ab2e3e7d1dbc7bc
ahmad jeffry aryanto <ahmad.aryanto00@gmail.com>
2019-12-09 21:33:29 +0700

    added justify content and button and a new file

---
+++ Index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP CRUD</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="row justify-content-center">
+	<form action="" method="Post">
+		<div class="form-group">
+		<label>Name</label>
+		<input type="text" name="name" class="form-control" value="Enter Your name">
+	</div>
+		<div class="form-group">
+		<label>Location</label>
+		<input type="text" name="Location" class="form-control" value="Enter Your location">
+	</div>
+		<div class="form-group">
+		<button type="submit" class="btn btn-primary" name="save" > Save</button>
+	</div>	
+
+	</form>
+</div>
+</html>
+</body>
