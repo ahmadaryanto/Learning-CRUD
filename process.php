d5b1be8260ff32b4a8dd2437aa2b83dd83170cf7
ahmadaryanto <ahmad.aryanto00@gmail.com>
2019-12-31 16:13:07 +0700

    added delete function sql

--- process.php
+++ process.php
@@ -8,3 +8,8 @@
 
  	$mysqli->query("INSERT INTO data (name,location) Values ('$name','$location')") or die ($mysqli->error);
  }
+
+ if(isset($_GET['delete'])){
+ 	$id = $_GET['delete'];
+ 	$mysqli->query("DELETE FROM data where id=$id") or die($mysqli->error());
+ }
