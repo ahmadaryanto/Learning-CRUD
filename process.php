93d0faaa23928332b9ff0a7014b3142df29efc5a
ahmad jeffry aryanto <ahmad.aryanto00@gmail.com>
2020-01-06 23:24:50 +0700

    addding message after deleting or saving

--- process.php
+++ process.php
@@ -6,10 +6,16 @@
  	$name= $_POST['name'];
  	$location= $_POST['location'];
 
+ 	$_SESSION['message'] = "Record has been saved!";
+ 	$_SESSION['msg_type'] = "Success";
+
  	$mysqli->query("INSERT INTO data (name,location) Values ('$name','$location')") or die ($mysqli->error);
  }
 
  if(isset($_GET['delete'])){
  	$id = $_GET['delete'];
  	$mysqli->query("DELETE FROM data where id=$id") or die($mysqli->error());
+
+ 	$_SESSION['message'] ="Record has been deleted";
+ 	$_SESSION['message'] ="danger";
  }
